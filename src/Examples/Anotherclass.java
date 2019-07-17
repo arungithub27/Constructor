b505d4d26666c824a4338114b69dce6769da86bb
arun <arunrecovery27@gmail.com>
2019-07-17 15:02:27 +0530

    Third

---
+++ src/Examples/Anotherclass.java
@@ -0,0 +1,14 @@
+package Examples;
+
+public class Anotherclass {
+	
+	public static void main(String[] args){
+		Myclass call = new Myclass(10,10);
+		call.calculate();
+		//System.out.println(call.calculate());
+		
+		
+		
+	}
+
+}
---
+++ src/Examples/Myclass.java
@@ -0,0 +1,16 @@
+package Examples;
+
+public class Myclass {
+	public int breadth;
+	public int width;
+	
+	Myclass(int b, int w){
+		breadth=b;
+	    width=w;
+	}
+   public int calculate(){
+	   System.out.println(breadth*width);
+	   return breadth*width;
+   }
+   
+}
